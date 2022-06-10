296da33f0fb2afca29952222228bb3efe6511472
Studio Kaori <studiokaori.com@gmail.com>
2022-06-10 12:15:53 +0200

    Initial Commit

---
+++ GitHubSearch/GitHubSearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubSearchApp.swift
+//  GitHubSearch
+//
+//  Created by Kaori Persson on 2022-06-10.
+//
+
+import SwiftUI
+
+@main
+struct GitHubSearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
