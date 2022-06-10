dad77fa2c5bdca70ac7e99d51411c0833d2b6dc8
Studio Kaori <studiokaori.com@gmail.com>
2022-06-10 12:33:05 +0200

    Add: image icon to the card

--- GitHubSearch/Views/CardView.swift
+++ GitHubSearch/Views/CardView.swift
@@ -19,6 +19,29 @@ struct CardView: View {
         let description: String?
         let url: String
     }
+    
+    let input: Input
+    
+    init(input: Input) {
+        self.input = input
+    }
+    
+    var body: some View {
+        VStack(alignment: .leading) {
+            
+            Image(uiImage: input.iconImage)
+                // not to change the color
+                .renderingMode(.original)
+                .resizable()
+                // Keep the aspect ration
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 60, height: 60)
+                .clipShape(Circle())
+                // Add border line
+                .overlay(Circle().stroke(Color.gray, lineWidth: 2))
+                .shadow(color: .gray, radius: 1, x: 0, y: 0)
+        }
+    }
 }
 
 struct CardView_Previews: PreviewProvider {
