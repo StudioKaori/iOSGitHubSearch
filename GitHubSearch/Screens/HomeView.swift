ee79536553d57d176a1986a2d1117135cc0e255e
Studio Kaori <studiokaori.com@gmail.com>
2022-06-10 12:56:34 +0200

    Add: NavigationView to homeview

---
+++ GitHubSearch/Screens/HomeView.swift
@@ -0,0 +1,42 @@
+//
+//  HomeView.swift
+//  GitHubSearch
+//
+//  Created by Kaori Persson on 2022-06-10.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    
+    // MARK: - Properties
+    
+    
+    // MARK: - Body
+    var body: some View {
+        
+        NavigationView {
+            
+            ScrollView(showsIndicators: false) {
+                
+            }
+            .padding()
+            .navigationBarTitle("", displayMode: .inline)
+            .navigationBarItems(
+                leading: HStack {
+                    TextField("Search...",
+                              text: $text,
+                              onCommit: {
+                        
+                    })
+                })
+        }
+        
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
