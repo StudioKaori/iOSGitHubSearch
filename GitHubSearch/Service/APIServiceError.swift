ffd229548e1810bf4c57a34397e55f64af8bef75
Studio Kaori <studiokaori.com@gmail.com>
2022-06-11 15:32:10 +0200

    Add: APIServiceError

---
+++ GitHubSearch/Service/APIServiceError.swift
@@ -0,0 +1,14 @@
+//
+//  APIServiceError.swift
+//  GitHubSearch
+//
+//  Created by Kaori Persson on 2022-06-11.
+//
+
+import Foundation
+
+enum APIServiceError: Error {
+    case invalidURL
+    case responseError
+    case parseError
+}
