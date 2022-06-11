28b591cf6351b954b315f369c86fe51fcd720168
Studio Kaori <studiokaori.com@gmail.com>
2022-06-11 15:42:55 +0200

    Add init to APIService

--- GitHubSearch/Service/APIService.swift
+++ GitHubSearch/Service/APIService.swift
@@ -11,3 +11,18 @@ import Combine
 protocol APIServiceType {
     func request<Request>(with request: Request) -> AnyPublisher<Request.Response, APIServiceError> where Request: APIRequestType
 }
+
+final class APIService: APIServiceType {
+    
+    private let baseURLString: String
+    
+    // initialise with baseURL
+    init(baseURLString: String = "https://api.github.com") {
+        self.baseURLString = baseURLString
+    }
+    
+    func request<Request>(with request: Request) -> AnyPublisher<Request.Response, APIServiceError> where Request : APIRequestType {
+        <#code#>
+    }
+    
+}
