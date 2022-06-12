1164ddda1521e5a1bb89c63e01bfd1ac4e1575cb
Studio Kaori <studiokaori.com@gmail.com>
2022-06-12 09:03:53 +0200

    Add:Inputs to homeVM

--- GitHubSearch/ViewModels/HomeViewModel.swift
+++ GitHubSearch/ViewModels/HomeViewModel.swift
@@ -6,3 +6,15 @@
 //
 
 import Foundation
+import Combine
+import UIKit
+
+final class HomeViewModel: ObservableObject {
+    
+    // MARK: - Input
+    enum Inputs {
+        // User finished to input
+        case onCommit(text: String)
+        case tappedCardView(urlString: String)
+    }
+}
